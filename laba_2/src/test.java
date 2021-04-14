cc789015eee2e5b6e2808cb6a4b53b9e07375de2
DrKleiner-ops <vlad_san98@mail.ru>
2021-04-14 15:58:24 +0500

    Apgrade test

--- laba_2/src/test.java
+++ laba_2/src/test.java
@@ -1,18 +1,37 @@
 import java.util.Scanner;
-
-
 public class test {
     public static void main(String[] args) {
-        Auto myAuto1=new Auto(); //создаем объект типа нашего класса
-        Scanner in = new Scanner(System.in); //создаем сканер для ввода данных из консоли
+        Auto myAuto1=new Auto();
+        Scanner in = new Scanner(System.in);
         System.out.print("Введите фирму: ");
-        String nazv=in.next(); //считываем название из консоли !!!только 1 слово
-//т.к. in.next() считывает только символы до пробела, остальные символы отправляет
-//следующему оператору, связанному с консольным вводом
-        myAuto1.setFirm(nazv); //задаем значение для параметра нашего класса
+        String nazv=in.next();
+        myAuto1.setFirm(nazv);
         System.out.print("Введите максимальную скорость: ");
         int s=in.nextInt();
         myAuto1.setMaxSpeed(s);
-        System.out.println(myAuto1.getFirm()+" "+myAuto1.getMaxSpeed());
+        System.out.println("Какой-то автомобиль: "+myAuto1.getFirm()+" "+myAuto1.getMaxSpeed());
+        System.out.println();
+        Car myCar1=new Car("Ford", 200,"Mustang",2,false);
+        Car myCar2=new Car();
+        System.out.print("Введите фирму-производитель легкового авто: ");
+        nazv=in.next();
+        myCar2.setFirm(nazv);
+        System.out.print("Введите максимальную скорость легкового авто: ");
+        s=in.nextInt();
+        myCar2.setMaxSpeed(s);
+        System.out.print("Введите модель легкового авто: ");
+        nazv=in.next();
+        myCar2.setModel(nazv);
+        System.out.print("Введите кол-во дверей легкового авто: ");
+        s=in.nextInt();
+        myCar2.setNumDoors(s);
+        System.out.print("Введите признак полного привода легкового авто (true/false): ");
+        Boolean f=in.nextBoolean();
+        System.out.println();
+        System.out.println("Первый легковой автомобиль: "+myCar1.toString());
+        System.out.println("Второй легковой автомобиль: "+myCar2.toString());
+        Truck myTruck=new Truck();
+        myTruck.setAllInfo();
+        System.out.println(myTruck.toString());
     }
 }
