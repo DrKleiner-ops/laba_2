e3371cae856a7a47eaf4f62e917c95ba2f159718
DrKleiner-ops <vlad_san98@mail.ru>
2021-04-15 16:16:15 +0500

    AllInfo

--- laba_2-2/src/firearm.java
+++ laba_2-2/src/firearm.java
@@ -20,6 +20,48 @@ public class firearm extends weapon {
         MagazineSize = size;
     }
 
+   public void setType(String typ){
+        Type=typ;
+   }
 
+   public String getType(){
+        return Type;
+   }
 
+    public void setModel(String mod){
+        Model=mod;
+    }
+
+    public String getModel(){
+        return Model;
+    }
+
+    public void setMagazineSize(int size){
+        MagazineSize=size;
+    }
+
+    public int getMagazineSize(){
+        return MagazineSize;
+    }
+
+    public void setAllInfo() {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите страну-производитель огнестрельного оружия: ");
+        String stran = in.next(); //метод next() позволяет вводить строки, но без пробелов
+        setProducerCountry(stran);
+        System.out.print("Введите тип огнестрельного оружия: ");
+        String tip = in.next(); //метод next() позволяет вводить строки, но без пробелов
+        System.out.print("Введите модель огнестрельного оружия: ");
+        String mo = in.next();
+        System.out.print("Серийный номер: ");
+        String ser= in.next();
+        setSerialNumber(ser);
+        System.out.print("Находится ли это оружие на вооружении? (true/false): ");
+        boolean v = in.nextBoolean();
+        setInService(v);
+        System.out.print("Введите размер магазина: ");
+        MagazineSize = in.nextInt();
+        System.out.println();
+    }
 }
