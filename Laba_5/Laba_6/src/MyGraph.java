43ce51c5cf20fa66c7897e97b6d4eab168dd69a5
DrKleiner-ops <vlad_san98@mail.ru>
2021-05-05 14:17:48 +0500

    Shapes

--- Laba_5/Laba_6/src/MyGraph.java
+++ Laba_5/Laba_6/src/MyGraph.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 public class MyGraph extends JFrame implements ActionListener {
     private JMyPanel myPanel = new JMyPanel();//объявляем и создаем нашу панель для рисования
@@ -10,7 +11,49 @@ public class MyGraph extends JFrame implements ActionListener {
     }
 
     public MyGraph(String s) { //конструктор с параметром для заголовка окна
+
+
         super(s);//вызываем конструктор суперкласса и передаем ему параметр
+        String[] sog = {"К","Л","М","Ш","Н"};
+        String[] gl = {"И"};
+        JMenuBar myMenuBar = new JMenuBar();// создаем строку меню
+        JMenu menu1 = new JMenu("Буквы");//создаем первый пункт меню
+        JMenu first = new JMenu("Согласные"); //создаем подпункт меню
+        menu1.add(first);//добавляем подпункт в первый пункт меню
+        JMenuItem[] first_1 = new JMenuItem[5];// создаем массив из трех подпунктов меню
+        for (int i = 0; i < 5; i++) { //в цикле создается каждый подпункт, добавляется в нужное место меню
+            first_1[i] = new JMenuItem(sog[i]);
+            first.add(first_1[i]);
+            first_1[i].addActionListener(this);//и к нему подключаем слушатель, описанный в конце
+        } //класса
+        JMenu second = new JMenu("Гласные"); //создаем подпункт меню
+        menu1.add(second); //добавляем его в меню
+        JMenuItem[] second_1 = new JMenuItem[1]; // создаем массив из трех подпунктов меню
+        for (int i = 0; i < 1; i++) { // и т.д.
+            second_1[i] = new JMenuItem(gl[i]);
+            second.add(second_1[i]);
+            second_1[i].addActionListener(this);
+        }
+        JMenu menu2 = new JMenu("Фигуры"); //создаем второй пункт меню и далее аналогично первому
+        JMenu first2 = new JMenu("Пункт2_1");
+        menu2.add(first2);
+        JMenuItem[] first2_1 = new JMenuItem[3];
+        for (int i = 0; i < 3; i++) {
+            first2_1[i] = new JMenuItem("Пункт2_1_" + (i + 1));
+            first2.add(first2_1[i]);
+            first2_1[i].addActionListener(this);
+        }
+        JMenu second2 = new JMenu("Пункт2_2");
+        menu2.add(second2);
+        JMenuItem[] second2_1 = new JMenuItem[3];
+        for (int i = 0; i < 3; i++) {
+            second2_1[i] = new JMenuItem("Пункт2_2_" + (i + 1));
+            second2.add(second2_1[i]);
+            second2_1[i].addActionListener(this);
+        }
+        myMenuBar.add(menu1); //в строку меню добавляем главные пункты меню
+        myMenuBar.add(menu2);
+        setJMenuBar(myMenuBar); //устанавливаем для окна созданное меню
         Box myBox = new Box(BoxLayout.X_AXIS);//создаем компоновку в виде горизонтального ящика
         JButton[] figs = new JButton[5]; //массив кнопок
         for (int i = 0; i < 5; i++) {
